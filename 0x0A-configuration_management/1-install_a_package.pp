f10cbb2878ab22b6bfb3bd29813b759a7f5f2a06
fikayo1 <soetandan@gmail.com>
2023-01-11 09:29:07 +0100

    puppet lint

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,6 @@
 # install flask from pip
 package { 'flask':
-  ensure => 'installed',
-  provider => 'pip3',
+  ensure          => 'installed',
+  provider        => 'pip3',
   install_options => '2.1.0'
 }
